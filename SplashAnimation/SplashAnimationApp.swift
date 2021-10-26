43dd2e42a5a463c9754abc0b3a9721f36f673310
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-26 20:11:23 +0600

    Initial Commit

---
+++ SplashAnimation/SplashAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashAnimationApp.swift
+//  SplashAnimation
+//
+//  Created by Sopnil Sohan on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SplashAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
